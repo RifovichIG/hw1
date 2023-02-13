1d7a685bd29cd79d27a42169fb9cbf99311ff24c
IldarG <Rifovich02@gmail.com>
2023-02-13 21:29:44 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ double a, b;
+        Console.Write("Введите первое число: ");
+        a=Double.Parse(Console.ReadLine ());
+        Console.Write("Введите второе число: ");
+        b=Double.Parse(Console.ReadLine ());
+        if (a > b)
+            Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a, b);
+        else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b, a);
+
+// Задача 2 требуется найти максимальное значение и минимальное//
+// где а=5 b=7 => min=5 max=7// где а=2 b=10 => min=2 max=10// где а=-9 b=-3 => min=-9 max=-3//
+
